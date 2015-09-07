031791d9829a99e449f71a3c3a8fb781128a73d8
Erik Trautman <erik.trautman@gmail.com>
2015-09-07 11:08:13 -0700

    add lots of comments

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,6 +1,8 @@
 todo.controller("TodoCtrl",
   ['$scope', '$window', 'todoCrud', function( $scope, $window, todoCrud ){
 
+
+    // Set up our initial items
     var item1 = {
       text: "Foo Item",
       dueDate: new Date(),
@@ -14,40 +16,50 @@ todo.controller("TodoCtrl",
     }
 
 
+    // Initialize variables
     $scope.items = [ item1, item2 ];
-    $scope.tester = "e";
     $scope.hideCompleted = false;
 
 
+    // Add an item to the collection
     $scope.addItem = function(){
 
       $window.console.log("Clicked name:" + $scope.item.text + " and due-date of " + $scope.item.dueDate + " and completed of " + $scope.item.completed );
 
+      // Use the model values attached to $scope to
+      //   construct the new item
       var newItem = {
         text: $scope.item.text,
         dueDate: $scope.item.dueDate,
         completed: $scope.item.completed || false,
       }
 
+      // Add the item to our collection
       $scope.items.push( newItem );
+
+      // Reset fields in our form
       $scope.item.text = undefined;
       $scope.item.dueDate = undefined;
+
     }
 
 
+    // Remove an item from our collection
+    //   by finding and splicing it out
     $scope.removeItem = function( item ){
       var index = $scope.items.indexOf( item );
       $scope.items.splice( index, 1 );
     }
 
 
+    // Change whether we will view all items or
+    //   only the uncompleted ones
     $scope.toggleCompleted = function(){
-      console.log("Toggling completion")
       $scope.hideCompleted = !$scope.hideCompleted;
-      console.log("hideCompleted: " + $scope.hideCompleted)
     }
 
 
+    // Remove all completed items from our collection
     $scope.destroyCompleted = function(){
       var newItems = []
       $scope.items.forEach(function(item){
@@ -57,21 +69,11 @@ todo.controller("TodoCtrl",
       })
       $scope.items = newItems;
     }
-    // $scope.toggleCompleted = function( item ){
-    //   $window.console.log(item);
-    //   $window.console.log( "toggling completed: " + item.completed );
-    //   item.completed = false;
-    //   $window.console.log( "toggled completed: " + item.completed );
-    // }
 
 
-    $scope.alert = alert.bind($window);
-
-
+    // Just a helper to test whether things are working
     $scope.test = function(){
-      // $scope.tester = "FOO";
       $window.console.log("HIHI");
-      $window.alert("Hello");
     }
 
   }]
