7a9832e098da61bfabb8f28858af76fd88e831db
Erik Trautman <erik.trautman@gmail.com>
2015-09-07 20:39:50 -0700

    refactor more directives and scope

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -2,45 +2,14 @@ todo.controller("TodoCtrl",
   ['$scope', '$window', 'todoCrud', function( $scope, $window, todoCrud ){
 
 
-    // Set up our initial items
-    var item1 = {
-      text: "Foo Item",
-      dueDate: new Date(),
-      completed: false,
-    }
-
-    var item2 = {
-      text: "Bar Item",
-      dueDate: new Date(),
-      completed: true,
-    }
-
-
     // Initialize variables
-    $scope.items = [ item1, item2 ];
-    $scope.hideCompleted = false;
+    $scope.items = todoCrud.getItems();
+    $scope.hideCompleted = todoCrud.hideCompleted;
 
 
     // Add an item to the collection
     $scope.addItem = function(){
-
-      $window.console.log("Clicked name:" + $scope.item.text + " and due-date of " + $scope.item.dueDate + " and completed of " + $scope.item.completed );
-
-      // Use the model values attached to $scope to
-      //   construct the new item
-      var newItem = {
-        text: $scope.item.text,
-        dueDate: $scope.item.dueDate,
-        completed: $scope.item.completed || false,
-      }
-
-      // Add the item to our collection
-      $scope.items.push( newItem );
-
-      // Reset fields in our form
-      $scope.item.text = undefined;
-      $scope.item.dueDate = undefined;
-
+      todoCrud.addItem( $scope.item )
     }
 
 
@@ -48,27 +17,20 @@ todo.controller("TodoCtrl",
     //   by finding and splicing it out
     $scope.removeItem = function( item ){
       console.log( "Destroying item: " + item );
-      var index = $scope.items.indexOf( item );
-      $scope.items.splice( index, 1 );
+      todoCrud.removeItem( item );
     }
 
 
     // Change whether we will view all items or
     //   only the uncompleted ones
     $scope.toggleCompleted = function(){
-      $scope.hideCompleted = !$scope.hideCompleted;
+      todoCrud.toggleCompleted();
     }
 
 
     // Remove all completed items from our collection
     $scope.destroyCompleted = function(){
-      var newItems = []
-      $scope.items.forEach(function(item){
-        if( !item.completed ){
-          newItems.push( item )
-        }
-      })
-      $scope.items = newItems;
+      todoCrud.destroyCompleted();
     }
 
 
--- services/todo_crud.js
+++ services/todo_crud.js
@@ -1,31 +1,87 @@
 todo.service('todoCrud', function(){
   
-  var sampleItem = { 
-      text: "Get groceries from the store",
+
+    // Set up our initial items
+    var item1 = {
+      text: "Foo Item",
       dueDate: new Date(),
-      completed: false 
+      completed: false,
     }
 
-  var obj = {};
-  obj.items = [ sampleItem ];
-
-  obj.test = function(){
-    console.log("HIHI");
-    alert("Hello");
-  }
-
-  obj.addItem = function(){
-    var newItem = {
-      text: $scope.text,
-      dueDate: $scope.dueDate,
+    var item2 = {
+      text: "Bar Item",
+      dueDate: new Date(),
+      completed: true,
     }
-    todoCrud.items.push( newItem );
-  }
 
-  obj.create = function(){ };
-  obj.index = function(){ };
-  obj.destroy = function( id ){ };
+    var items = [ item1, item2 ];
+    var hideCompleted = false
 
-  return obj;
+
+    var getItems = function(){
+      return items;
+    }
+
+
+    var addItem = function( item ){
+
+      // Use the model values attached to $scope to
+      //   construct the new item
+      var newItem = {
+        text: item.text,
+        dueDate: item.dueDate,
+        completed: item.completed || false,
+      }
+
+      // Add the item to our collection
+      items.push( newItem );
+
+      // Reset fields in our form
+      item.text = undefined;
+      item.dueDate = undefined;
+
+      // Send back the newly reset item
+      return item;
+    }
+
+
+    // Remove an Item from the collection
+    var removeItem = function( item ){
+      var index = items.indexOf( item );
+      items.splice( index, 1 );
+    }
+
+
+    // Change whether to show completed 
+    //   To-Do items or not
+    var toggleCompleted = function(){
+      hideCompleted = !hideCompleted;
+    }
+
+
+    // This function MUST be destructive to
+    //   the ORIGINAL `items` array, not 
+    //   creating a new one and returning it.
+    // If we did that, the $digest loop would
+    //   lose track of it.
+    var destroyCompleted = function(){
+      items.forEach(function(item){
+        if( item.completed ){
+          removeItem( item );
+        }
+      })
+    }
+
+
+
+  // Use a revealing module
+  return {
+    getItems:         getItems,
+    hideCompleted:    hideCompleted,
+    addItem:          addItem,
+    removeItem:       removeItem,
+    toggleCompleted:  toggleCompleted,
+    destroyCompleted: destroyCompleted,
+  };
 
 })
